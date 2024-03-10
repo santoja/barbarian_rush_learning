f2bbb48bf57968b1c4a5a492af7dc69a90e9b73e
Kaue Santoja <shinjiiraki@gmail.com>
2024-03-10 18:46:38 +0100

    Added check for distance between turret and enemy

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,17 +1,35 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@export var turret_range := 10.0
 
 @onready var barrel: MeshInstance3D = $TurretBase/TurretTop/Barrel
 
 var enemy_path: Path3D 
+var target: PathFollow3D
 
 func _physics_process(delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	target = find_best_target()
+	if target:
+		look_at(target.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
+	if target == null:
+		return
 	var shot = projectile.instantiate()
 	add_child(shot)
 	shot.global_position = barrel.global_position
 	shot.direction = global_transform.basis.z
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progres = 0
+	for enemy in enemy_path.get_children():
+		if not enemy is PathFollow3D:
+			continue
+		if global_position.distance_to(enemy.global_position) > turret_range:
+			continue
+		if enemy.progress > best_progres:
+			best_progres = enemy.progress
+			best_target = enemy
+	return best_target 
