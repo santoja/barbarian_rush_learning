7716b6e9cd864e05b4681ee9119ecd6d3a74e004
Kaue Santoja <shinjiiraki@gmail.com>
2024-03-11 16:05:02 +0100

    Finished the project and exported for windows

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -3,8 +3,10 @@ extends Node3D
 @export var projectile: PackedScene
 @export var turret_range := 10.0
 
-@onready var barrel: MeshInstance3D = $TurretBase/TurretTop/Barrel
+@onready var barrel: Node3D = $TurretBase/TurretTop
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var cannon: Node3D = $TurretBase/TurretTop/Cannon
+@onready var turret_base: Node3D = $TurretBase
 
 var enemy_path: Path3D 
 var target: PathFollow3D
@@ -12,7 +14,7 @@ var target: PathFollow3D
 func _physics_process(delta: float) -> void:
 	target = find_best_target()
 	if target:
-		look_at(target.global_position, Vector3.UP, true)
+		turret_base.look_at(target.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
 	if target == null:
@@ -20,8 +22,8 @@ func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
 	animation_player.play("turret_fire")
-	shot.global_position = barrel.global_position
-	shot.direction = global_transform.basis.z
+	shot.global_position = cannon.global_position
+	shot.direction = turret_base.global_transform.basis.z
 
 func find_best_target() -> PathFollow3D:
 	var best_target = null
